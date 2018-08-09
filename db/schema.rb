9ec597e9c0353a41ea3c70dc896f9f4483184a0a
Enzo Mathe <enzomathe@gmail.com>
2018-08-09 10:05:06 +0200

    Problems with model validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_09_073408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cocktails", force: :cascade do |t|
+    t.text "name"
+    t.bigint "dose_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dose_id"], name: "index_cocktails_on_dose_id"
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "cocktails", "doses"
 end
