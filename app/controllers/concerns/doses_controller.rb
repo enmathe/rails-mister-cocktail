afdb6869d4cf14087b1dc26740d10efc98d838ea
Enzo Mathe <enzomathe@gmail.com>
2018-08-09 15:31:30 +0200

    new dose to check

--- app/controllers/concerns/doses_controller.rb
+++ app/controllers/concerns/doses_controller.rb
@@ -5,21 +5,15 @@ class DosesController < ApplicationController
   end
 
   def create
-    # On récupère le cocktail auquel va être associée la dose
     @cocktail = Cocktail.find(params[:cocktail_id])
 
-    # On instancie un objet dose
     @dose  = Dose.new(dose_params)
 
-    # Associer la dose à son cocktail
     @dose.cocktail = @cocktail
 
-    # save
     if @dose.save
-      # si ça marche -> redirige sur la show du cocktail
       redirect_to cocktail_path(@cocktail)
     else
-      # sinon -> render le form de création de la dosee
       render :new
     end
   end
@@ -27,8 +21,6 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
 
-    # On se met de côté le garden pour pouvoir
-    # redirect le user vers lui après la destruction de la dose
     garden = @dose.garden
 
     @dose.destroy
