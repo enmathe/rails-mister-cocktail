12b08819b28c130bb1aea91239e638fe6985b822
Enzo Mathe <enzomathe@gmail.com>
2018-08-09 11:34:53 +0200

    Need to check no destroy if has dose chlidren

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,10 +1,5 @@
 class Ingredient < ApplicationRecord
+
   has_many :doses, dependent: :destroy
   validates :name, presence: true,  allow_blank: false, uniqueness: true
-
-  before_destroy :check
-
-  def check
-    return self.dose.empty?
-  end
 end
